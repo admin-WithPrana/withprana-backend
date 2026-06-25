ecec0d05902adf9a0b05122bd00f51aee7c6bf9a
xt900 <x2cvicious123@gmail.com>
2026-06-25 21:31:02 +0530

    refresh token and token issue fix

--- src/interfaces/controllers/userController.js
+++ src/interfaces/controllers/userController.js
@@ -79,6 +79,7 @@ export class UserController {
         success: result.success,
         message: result.message,
         token: result.token,
+        refreshToken: result.refreshToken,
         oauth: result.oauth,
         register: result.register,
       });
